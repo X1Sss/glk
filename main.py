33d24ecdae770e0daf5d9aa523ae99c2d5a48977
X1SS <egor12345671gar@gmail.com>
2023-11-22 00:08:51 +0700

    i added file, who created yellow circles, design is UI.ui

---
+++ main.py
@@ -0,0 +1,39 @@
+from PyQt5 import QtWidgets, uic
+import sys
+import random
+
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
+from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
+
+
+class generate_circles(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(generate_circles, self).__init__()
+        uic.loadUi('UI.ui', self)
+        self.show()
+        self.event_draw = 0
+        self.create_circl.clicked.connect(self.paintcircle)
+        self.should_paint_circle = False
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if self.should_paint_circle:
+            painter = QtGui.QPainter(self)
+            painter.setRenderHint(QPainter.Antialiasing)
+            painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+            for a in range(random.randint(1, 10)):
+                painter.drawEllipse(random.randint(0, 400), random.randint(0, 400),
+                                    random.randint(0, 400), random.randint(0, 400))
+
+    def paintcircle(self):
+        self.should_paint_circle = True
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = generate_circles()
+    ex.show()
+    sys.exit(app.exec())
